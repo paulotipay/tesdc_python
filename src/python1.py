68cb33c2cff4da5e74061dc330e704b7372ad4a2
paulo <paulo@kodego.ph>
2022-09-25 12:18:53 +0800

    First commit

---
+++ src/python1.py
@@ -0,0 +1,21 @@
+# The line below will print Hello Python!
+print("Hello Python!")
+
+# The line below will print Hello
+print('Hello')
+
+# The line below will print "Hello"
+print('"Hello"')
+
+# The line below will print 'Hello'
+print("'Hello'")
+
+# The line below will print the following
+# Hello
+# from
+# python!
+print("""
+Hello
+from
+python
+""")
