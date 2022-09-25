65a89390afe2ca498ba0eed9b2b267ec894e047e
paulo <paulo@kodego.ph>
2022-09-25 13:19:06 +0800

    commit python3

---
+++ src/python3.py
@@ -0,0 +1,17 @@
+# changing variable value and checking data type
+# create a variable and assign 10
+x = 10
+# check the data type
+print(type(x))
+
+# reassign value from 10 to Hello
+x = "Hello"
+print(type(x))
+
+# reassign value from "Hello" to Boolean True
+x = True
+print(type(x))
+
+# reassign value from Boolean True to float 3.14
+x = 3.14
+print(type(x))
