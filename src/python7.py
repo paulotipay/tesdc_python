85af46310b1b884c8f785e27ea39bc875c8eae4a
paulo <paulo@kodego.ph>
2022-09-25 13:02:16 +0800

    remove all

--- src/python7.py
+++
@@ -1,8 +0,0 @@
-# accepting input
-name = input("Enter your name: ")
-number1 = input("Enter number 1:")
-number2 = input("Enter number 2:")
-print(f'Your name is {name}')
-print(f'The sum of {number1} and {number2} is {int(number1)+int(number2)}')
-# float()
-# str()
