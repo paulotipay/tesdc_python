58dd716c0d23e78e47fad8639a77b2d7c2c56f77
paulo <paulo@kodego.ph>
2022-09-25 12:56:16 +0800

    commit python7.py

---
+++ src/python7.py
@@ -0,0 +1,8 @@
+# accepting input
+name = input("Enter your name: ")
+number1 = input("Enter number 1:")
+number2 = input("Enter number 2:")
+print(f'Your name is {name}')
+print(f'The sum of {number1} and {number2} is {int(number1)+int(number2)}')
+# float()
+# str()
